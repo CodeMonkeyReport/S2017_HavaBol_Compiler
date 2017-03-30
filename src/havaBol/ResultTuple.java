e6bb7770b7a6a7e4e6f5a0cecfb0b2ae7c0ab026
CodeMonkeyReport <michael.alexander.geyer@gmail.com>
2017-03-30 14:11:14 -0500

    Tuple symbol table class

--- src/havaBol/ResultTuple.java
+++ src/havaBol/ResultTuple.java
@@ -3,4 +3,11 @@ package havaBol;
 public class ResultTuple extends ResultValue
 {
 
+		public ResultTuple(String type)
+		{
+			super(type);
+			// In this case our constructor will need more information.
+				// Number of elements
+				// type of elements
+		}
 }
