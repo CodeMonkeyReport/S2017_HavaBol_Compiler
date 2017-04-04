05b29e39f9a857bc70ac8ed4e1659fe793992cd6
Zacharius <zee5229@gmail.com>
2017-04-04 15:11:10 -0500

    Tests made, Maxlength removed, Elem functions dont work, problem arrays
    Structure not being set properly in array

--- src/test/FunctionTest.java
+++ src/test/FunctionTest.java
@@ -17,7 +17,7 @@ public class FunctionTest {
 
 	
 	@Test
-	public void simpleFunctionCallInExpression()
+	public void emptySpacesTest()
 	{
 		// Set up the inital 'file' to be read
 		String testInput = "String empty;\n"
@@ -44,4 +44,124 @@ public class FunctionTest {
 			assertTrue(false);
 		}
 	}
+	
+	@Test
+	public void nonemptySpacesTest()
+	{
+		// Set up the inital 'file' to be read
+		String testInput = "String empty;\n"
+						 + "empty = 'not empty';\n"
+						 + "Bool false = SPACES(empty);\n";
+		
+		StringReader testReader = new StringReader(testInput);
+		BufferedReader br = new BufferedReader(testReader);
+		SymbolTable st = new SymbolTable();
+		StorageManager storageManager = new StorageManager();
+		try {
+			Scanner testScanner = new Scanner("TEST", br, st);
+			Parser parser = new Parser(testScanner, storageManager);
+			
+			// Use this area to run tests	
+			parser.statements(true);
+			
+			assertEquals("F", storageManager.getVariableValue("false").internalValue);
+			
+			//***
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			e.printStackTrace();
+			assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void lengthTest()
+	{
+		// Set up the inital 'file' to be read
+		String testInput = "String length;\n"
+						 + "length =  'Hello World!';\n"
+						 + "Int x = LENGTH(length);\n";
+		
+		StringReader testReader = new StringReader(testInput);
+		BufferedReader br = new BufferedReader(testReader);
+		SymbolTable st = new SymbolTable();
+		StorageManager storageManager = new StorageManager();
+		try {
+			Scanner testScanner = new Scanner("TEST", br, st);
+			Parser parser = new Parser(testScanner, storageManager);
+			
+			// Use this area to run tests	
+			parser.statements(true);
+			
+			assertEquals("12", storageManager.getVariableValue("x").internalValue);
+			
+			//***
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			e.printStackTrace();
+			assertTrue(false);
+		}
+	}
+	
+	
+	
+	@Test
+	public void elemTest()
+	{
+		// Set up the inital 'file' to be read
+		String testInput = "Int array[10] = 2,6,10;\n"
+						 + "Int x = ELEM(array);\n";
+		
+		StringReader testReader = new StringReader(testInput);
+		BufferedReader br = new BufferedReader(testReader);
+		SymbolTable st = new SymbolTable();
+		StorageManager storageManager = new StorageManager();
+		try {
+			Scanner testScanner = new Scanner("TEST", br, st);
+			Parser parser = new Parser(testScanner, storageManager);
+			
+			// Use this area to run tests	
+			parser.statements(true);
+			
+			assertEquals("3", storageManager.getVariableValue("x").internalValue);
+			
+			//***
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			e.printStackTrace();
+			assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void maxElemTest()
+	{
+		// Set up the inital 'file' to be read
+		String testInput = "Int array[10] = 2,6,10;\n"
+						 + "nt x = MAXELEM(array);\n";
+		
+		StringReader testReader = new StringReader(testInput);
+		BufferedReader br = new BufferedReader(testReader);
+		SymbolTable st = new SymbolTable();
+		StorageManager storageManager = new StorageManager();
+		try {
+			Scanner testScanner = new Scanner("TEST", br, st);
+			Parser parser = new Parser(testScanner, storageManager);
+			
+			// Use this area to run tests	
+			parser.statements(true);
+			
+			assertEquals("10", storageManager.getVariableValue("x").internalValue);
+			
+			//***
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			e.printStackTrace();
+			assertTrue(false);
+		}
+	}
+	
+	
+	
+	
 }
